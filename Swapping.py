e37ec8f6f9d7d6444c4152e1bc422c741cb80199
ShreyaMV <shreyamiriyamv@gmail.com>
2026-07-28 14:47:13 +0400

    Swapping Two Numbers

---
+++ Swapping.py
@@ -0,0 +1,9 @@
+print("Before Swapping")
+first_number = int(input("First Number : "))
+second_number = int(input("Second Number : "))
+temp = first_number
+first_number = second_number
+second_number = temp
+print("After Swapping")
+print(f"First Number : {first_number}")
+print(f"Second Number : {second_number}")
