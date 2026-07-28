2c5f0a6e43a6662005a241caee09e8c35a7302f5
ShreyaMV <shreyamiriyamv@gmail.com>
2026-07-28 14:46:50 +0400

    Finding total and average marks

---
+++ Marks.py
@@ -0,0 +1,7 @@
+sub_1 = int(input("Subject 1 : "))
+sub_2 = int(input("Subject 2 : "))
+sub_3 = int(input("Subject 3 : "))
+total = sub_1 + sub_2 + sub_3
+average = round((total/3), 2)
+print(f"Total : {total}")
+print(f"Average : {average}")
